0b05fc053c6af7add31cd7fc000cebccaf158bf2
miker2004 <157730940+miker2004@users.noreply.github.com>
2025-01-04 15:36:33 +0100

    test2

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,13 +1,13 @@
 
 import FilterProduct from "../components/products/FilterProduct";
-// import Searchbar from "../components/products/Searchbar";
+import Searchbar from "../components/products/Searchbar";
 import ShowMore from "../components/products/ShowMore";
 import ShowProducts from "../components/products/ShowProducts";
 import '../css/productPage.css'
 const Products = () => {
   return(
     <>
-    {/* <Searchbar/> */}
+    <Searchbar/>
     <FilterProduct/>
     <ShowProducts/>
     <ShowMore/>
