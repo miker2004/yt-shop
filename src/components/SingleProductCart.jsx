a04546f3115bb2587ae970d48db3815884ac65dd
miker2004 <157730940+miker2004@users.noreply.github.com>
2025-01-05 17:51:02 +0100

    Products from json

--- src/components/SingleProductCart.jsx
+++ src/components/SingleProductCart.jsx
@@ -1,26 +1,49 @@
 import { Box, Button, Typography } from "@mui/material";
-import '../css/singleproduct.css'
+import "../css/singleproduct.css";
 import { Link } from "react-router-dom";
+import products from "../products.json";
 
+const SingleProductCart = ({ limit }) => {
+  const limitedProducts = limit ? products.products.slice(0, limit) : products.products;
 
-const SingleProductCart = () => {
-  return(
-    <Box className="featured-products-box">
-    <Box className="featured-products-img">Pencil IMG</Box>
-    <Box className="featured-products-details">
-      <Typography  component="h2" className="featured-products-details-name">Pencil</Typography>
-      <Typography  component="h3">$6.99</Typography>
-      <Button
-        variant="contained"
-        className="featured-products-button"
-        component={Link}
-        to="/products/id"
-      >
-        Check Out
-      </Button>
+  return (
+    <Box className="featured-products-layout">
+  {limitedProducts.map((product, index) => (
+    <Box key={index} className="featured-products-box">
+    <Box key={index} className="featured-products-item">
+      <Box className="featured-products-img">
+        {/* <img
+          src={`path_to_image/${product.name.toLowerCase()}.jpg`}
+          alt={product.name}
+          className="product-image"
+        /> */}
+      </Box>
+      <Box className="featured-products-details">
+        <Typography
+          component="h2"
+          className="featured-products-details-name"
+        >
+          {product.name}
+        </Typography>
+        <Typography component="h3" className="product-price">
+          ${product.price}
+        </Typography>
+        <Button
+          variant="contained"
+          className="featured-products-button"
+          component={Link}
+          to={`/products/${product.name
+            .toLowerCase()
+            .replace(/\s+/g, "-")}`}
+        >
+          Check Out
+        </Button>
+      </Box>
     </Box>
-  </Box>
+    </Box>
+  ))}
+</Box>
   );
-}
+};
 
 export default SingleProductCart;
--- src/components/homepage/FeaturedProducts.jsx
+++ src/components/homepage/FeaturedProducts.jsx
@@ -2,15 +2,14 @@ import { Box, Typography } from "@mui/material";
 import SingleProductCart from "../SingleProductCart";
 
 const FeaturedProducts = () => {
+
   return (
     <Box className="featured-products">
-      <Typography  component="h3" className="featured-products-title">
+      <Typography component="h3" className="featured-products-title">
         BESTSELLERS
       </Typography>
-          <SingleProductCart/>
-          <SingleProductCart/>
-          <SingleProductCart/>
-        </Box>
+          <SingleProductCart limit={3}/>
+    </Box>
   );
 };
 
